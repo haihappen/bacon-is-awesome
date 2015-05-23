33d15b4a6a7e1acaed2a46bc68399d25d9f62ed3
Mario Uher <uher.mario@gmail.com>
2015-05-23 08:56:40 +0200

    Removes workarounds for jQuery.

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,3 @@
-var webpack = require('webpack');
-
-
 module.exports = {
   entry: './index.js',
   module: {
@@ -10,14 +7,6 @@ module.exports = {
       loader: 'babel?stage=0&loose[]=es6.modules&loose[]=es6.classes'
     }]
   },
-  plugins: [
-    new webpack.ProvidePlugin({
-      $: 'jquery',
-      jQuery: 'jquery',
-      'window.jQuery': 'jquery',
-      'root.jQuery': 'jquery'
-    })
-  ],
   output: {
     filename: 'bundle.js'
   }
