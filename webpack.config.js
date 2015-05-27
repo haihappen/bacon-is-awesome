d010984689a231f918a0ec495b288294e87eab35
Mario Uher <uher.mario@gmail.com>
2015-05-27 13:59:37 +0200

    Better readability for webpack.config.js.

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,12 @@
 module.exports = {
   entry: './index.js',
-  module: {
-    loaders: [{
-      test: /\.jsx?$/,
-      exclude: /node_modules/,
-      loader: 'babel?stage=0&loose[]=es6.modules&loose[]=es6.classes'
-    }]
-  },
   output: {
     filename: 'bundle.js'
+  },
+
+  module: {
+    loaders: [
+      { test: /\.jsx?$/, exclude: /node_modules/, loader: 'babel?stage=0&loose[]=es6.modules&loose[]=es6.classes' }
+    ]
   }
 };
